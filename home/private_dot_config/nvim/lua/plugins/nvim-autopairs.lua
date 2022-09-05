3d537aac588ce65c280496a866d6619c13dfb9db
Danilo Ribeiro da Silveira <danilo@vebi.com.br>
2022-09-05 08:20:14 -0300

    Update external

--- home/private_dot_config/nvim/lua/plugins/nvim-autopairs.lua
+++
@@ -1,8 +0,0 @@
------------------------------------------------------------
--- Nvim Autopairs configuration file
------------------------------------------------------------
-
--- Plugin: nvim-autopairs
--- url: https://github.com/windwp/nvim-autopairs
-
-require('nvim-autopairs').setup{}
